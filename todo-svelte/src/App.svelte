f6a4c5da36a1cb5253f5381307124dbbc1ce194a
Saurabh Kumar <saurabh.k0402@gmail.com>
2021-09-08 20:55:17 +0530

    Make a simple todo

--- todo-svelte/src/App.svelte
+++ todo-svelte/src/App.svelte
@@ -1,1 +1,55 @@
-<h1>Hello World!</h1>
+<script>
+  import uuid from 'uuid-v4';
+
+  let newTodo = '';
+  $: todos = [];
+
+  function toggleTask(id) {
+    todos = todos.map((todo) => {
+      if (todo.id !== id) {
+        return todo;
+      }
+
+      return {
+        ...todo,
+        done: !todo.done,
+      };
+    });
+  }
+
+  function addToDo() {
+    todos = [
+      ...todos,
+      {
+        id: uuid(),
+        text: newTodo,
+        done: false,
+      },
+    ];
+
+    newTodo = '';
+  }
+</script>
+
+<div>
+  <input type="text" bind:value={newTodo} placeholder="I want to do this" />
+  <button on:click|preventDefault={addToDo}>Add To Do</button>
+</div>
+{#if !todos.length}
+  <h3>Let's do something awesome!</h3>
+{:else}
+  {#each todos as todo}
+    <div
+      class={todo.done ? 'done' : 'pending'}
+      on:click={() => toggleTask(todo.id)}
+    >
+      {todo.text}
+    </div>
+  {/each}
+{/if}
+
+<style>
+  .done {
+    text-decoration: line-through;
+  }
+</style>
