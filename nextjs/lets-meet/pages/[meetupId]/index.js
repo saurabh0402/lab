14087bc887654f4e42464d9d9650f861cec71462
Saurabh Kumar <saurabh.k0402@gmail.com>
2021-12-11 20:53:01 +0530

    Meetup app with static data

--- nextjs/lets-meet/pages/[meetupId]/index.js
+++ nextjs/lets-meet/pages/[meetupId]/index.js
@@ -2,21 +2,52 @@ import { useRouter } from 'next/router';
 
 import MeetupDetails from '../../components/meetups/MeetupDetails';
 
-function Details() {
+const MEETUP = {
+  id: 1,
+  image: 'https://images.ctfassets.net/hspc7zpa5cvq/2R4dw4464nMTeqnZs4DemF/deb48860f73cb5228fe4fc7c293fbad4/JSConf_US.png',
+  title: 'JS Conf',
+  address: 'Carlsbad, CA',
+  description: 'An awesone conference about all things JavaScript',
+};
+
+function Details(props) {
   const router = useRouter();
   const meetupId = router.query.meetupId;
 
-  const details = {
-    id: 1,
-    image: 'https://images.ctfassets.net/hspc7zpa5cvq/2R4dw4464nMTeqnZs4DemF/deb48860f73cb5228fe4fc7c293fbad4/JSConf_US.png',
-    title: 'JS Conf',
-    address: 'Carlsbad, CA',
-    description: 'An awesone conference about all things JavaScript',
-  };
+  console.log(meetupId);
 
   return (
-    <MeetupDetails {...details} />
+    <MeetupDetails {...props.meetup} />
   )
+}
+
+export async function getStaticPaths() {
+  // Must be used when we have getStaticProps in a dynamic page
+  // This should return all the dynamic paths so that static pages can be generated for all of them.
+
+  // This will normally not be hardcoded but generated after fetching data from some DB.
+
+  return {
+    paths: [
+      {
+        params: {
+          meetupId: '1',
+        },
+      }
+    ],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  return {
+    props: {
+      meetup: {
+        ...MEETUP,
+        id: context.params.meetupId,
+      },
+    },
+  };
 }
 
 export default Details;
--- nextjs/lets-meet/pages/index.js
+++ nextjs/lets-meet/pages/index.js
@@ -1,24 +1,51 @@
 import MeetupList from "../components/meetups/MeetupList";
 
-function Home(){
-  const meetups = [
-    {
-      id: 1,
-      image: 'https://images.ctfassets.net/hspc7zpa5cvq/2R4dw4464nMTeqnZs4DemF/deb48860f73cb5228fe4fc7c293fbad4/JSConf_US.png',
-      title: 'JS Conf',
-      address: 'Carlsbad, CA',
-    },
-    {
-      id: 2,
-      image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--8DZt4NEW--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/j3b9qv0qq35a7s8on0dn.png',
-      title: 'React Conf',
-      address: 'Somewhere in world',
-    },
-  ];
+const MEETUPS = [
+  {
+    id: 1,
+    image: 'https://images.ctfassets.net/hspc7zpa5cvq/2R4dw4464nMTeqnZs4DemF/deb48860f73cb5228fe4fc7c293fbad4/JSConf_US.png',
+    title: 'JS Conf',
+    address: 'Carlsbad, CA',
+  },
+  {
+    id: 2,
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--8DZt4NEW--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/j3b9qv0qq35a7s8on0dn.png',
+    title: 'React Conf',
+    address: 'Somewhere in world',
+  },
+];
 
+function Home(props){
   return (
-    <MeetupList meetups={meetups} />
+    <MeetupList meetups={props.meetups} />
   );
+}
+
+// export async function getServerSideProps(context) {
+//   // This runs on server on every request
+
+//   const req = context.req;
+//   const res = context.res;
+
+//   console.log(req, res);
+
+//   return {
+//     props: {
+//       meetups: MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  // This is called during build process, and the data is "cached".
+  // revalidate property tells it to refetch data after given seconds
+
+  return {
+    props: {
+      meetups: MEETUPS,
+    },
+    revalidate: 3600,
+  };
 }
 
 export default Home;
