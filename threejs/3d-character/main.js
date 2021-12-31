2038c26bd593030031f07e85e8a1b4fc0dbdaa03
Saurabh Kumar <saurabh.k0402@gmail.com>
2021-12-31 21:41:26 +0530

    Add a few parts to the character

--- threejs/3d-character/main.js
+++ threejs/3d-character/main.js
@@ -2,6 +2,58 @@ function degreeToRadian(degress) {
   return degress * (Math.PI / 180);
 }
 
+class Figure {
+  constructor(params, scene) {
+    this.params = {
+      x: 0,
+      y: 0,
+      z: 0,
+      rx: 0,
+      ry: 0,
+      ...params,
+    };
+
+    this.group = new THREE.Group()
+    scene.add(this.group);
+  }
+
+  createBody() {
+    const geometry = new THREE.BoxGeometry(1, 1.5, 1);
+    const body = new THREE.Mesh(geometry, this.params.material);
+    this.group.add(body);
+  }
+
+  createHead() {
+    const geometry = new THREE.BoxGeometry(1.4, 1.4, 1.4);
+    const head = new THREE.Mesh(geometry, this.params.material);
+    this.group.add(head);
+
+    head.position.y = 1.65;
+  }
+
+  createHands() {
+    const geometry = new THREE.BoxGeometry(0.25, 1.5, 0.25);
+
+    const hand1 = new THREE.Mesh(geometry, this.params.material);
+    const hand2 = new THREE.Mesh(geometry, this.params.material);
+
+    hand1.position.x = 0.8;
+    hand1.position.y = 0.1;
+
+    hand2.position.x = -0.8;
+    hand2.position.y = 0.1;
+
+    this.group.add(hand1);
+    this.group.add(hand2);
+  }
+
+  init(material) {
+    this.createBody();
+    this.createHead();
+    this.createHands();
+  }
+}
+
 (() => {
   const canvas = document.querySelector('#planet');
 
@@ -10,12 +62,9 @@ function degreeToRadian(degress) {
   camera.position.z = 5;
   scene.add(camera);
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
   const material = new THREE.MeshLambertMaterial({ color: 0xffffff });
-  const mesh = new THREE.Mesh(geometry, material);
-  mesh.rotation.x = degreeToRadian(30);
-  mesh.rotation.y = degreeToRadian(30);
-  scene.add(mesh);
+  const character = new Figure({ material }, scene);
+  character.init();
 
   const ambientLight = new THREE.AmbientLight(0x9eaeff, 0.2);
   scene.add(ambientLight);
