be6ad5f940047ee8d29e618242bf0d09c55583a7
Saurabh Kumar <saurabh.k0402@gmail.com>
2022-01-09 15:45:29 +0530

    Convert rabbitmq example to use promises

--- rabbitmq/hello-world/receiver.js
+++ rabbitmq/hello-world/receiver.js
@@ -1,32 +1,33 @@
 #!/usr/bin/env node
 
-const amqp = require('amqplib/callback_api');
+const amqp = require('amqplib');
+
+async function main() {
+  let connection = null;
+
+  try {
+    connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    const queue = 'hello-world';
+
+    channel.assertQueue(queue, {
+      durable: false
+    });
+
+    console.log('Waiting for messages now 🤞');
+    channel.consume(queue, (msg) => {
+      console.log(`➡️ ${msg.content.toString()}`);
+    }, {
+      noAck: true,
+    });
+  } catch(err) {
+    connection.close();
+    console.error(err);
+  }
+}
 
 if (require.main === module) {
-  amqp.connect('amqp://localhost', (connErr, connection) => {
-    if (connErr) {
-      console.log('Unable to connect to RabbitMQ 😢');
-      process.exit(1);
-    }
-
-    connection.createChannel((channelErr, channel) => {
-      if (channelErr) {
-        console.log('Unable to create channel 😢');
-        process.exit(1);
-      }
-
-      const queue = 'hello-world';
-      
-      channel.assertQueue(queue, {
-        durable: false,
-      });
-
-      console.log('Waiting for messages now 🤞');
-      channel.consume(queue, (msg) => {
-        console.log(`➡️ ${msg.content.toString()}`);
-      }, {
-        noAck: true,
-      });
-    });
-  });
+  main()
+    .catch(console.error);
 }
--- rabbitmq/hello-world/sender.js
+++ rabbitmq/hello-world/sender.js
@@ -1,33 +1,33 @@
 #!/usr/bin/env node
 
-const amqp = require('amqplib/callback_api');
+const amqp = require('amqplib');
+
+async function main() {
+  let connection = null;
+  try {
+    connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    const queue = 'hello-world';
+    const message = Buffer.from('Hello RabbitMQ');
+
+    channel.assertQueue(queue, {
+      durable: false
+    });
+
+    channel.sendToQueue(queue, message);
+    console.log('✅ Message Sent!');
+
+    setTimeout(() => {
+      connection.close();
+    }, 1000);
+  } catch(err) {
+    connection?.close();
+    console.error(err);
+  }
+}
 
 if (require.main === module) {
-  amqp.connect('amqp://localhost', (connErr, connection) => {
-    if (connErr) {
-      console.log('Unable to connect to RabbitMQ 😢');
-      process.exit(1);
-    }
-
-    connection.createChannel((channelErr, channel) => {
-      if (channelErr) {
-        console.log('Unable to create channel 😢');
-        process.exit(1);
-      }
-
-      const queue = 'hello-world';
-      const message = Buffer.from('Hello RabbitMQ');
-
-      channel.assertQueue(queue, {
-        durable: false,
-      });
-
-      channel.sendToQueue(queue, message);
-      console.log('✅ Message Sent!');
-
-      setTimeout(() => {
-        connection.close();
-      }, 1000);
-    });
-  });
+  main()
+    .catch(console.error);
 }
