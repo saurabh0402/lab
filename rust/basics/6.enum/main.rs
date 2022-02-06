d7cf2cb3c874043b506f1081621549f8eac80ec2
Saurabh Kumar <saurabh.k0402@gmail.com>
2022-02-06 12:51:01 +0530

    Rust enums

---
+++ rust/basics/6.enum/main.rs
@@ -0,0 +1,70 @@
+#[derive(Debug)]
+enum Identity {
+  Passport(u32),
+  Aadhaar(String),
+}
+
+struct Person {
+  name: String,
+  last_name: String,
+  age: u32,
+  id: Identity,
+}
+
+struct Animal (String, u32);
+
+impl Person {
+  fn print(&self) {
+    println!("{} {} - {} - {:?}", self.name, self.last_name, self.age, self.id);
+  }
+}
+
+fn  check_identity(id: Identity) {
+  if let Identity::Passport(passport_id) = id {
+    println!("User gave passport with ID - {}", passport_id);
+  } else {
+    println!("User does not have a passport");
+  }
+
+  match id {
+    Identity::Aadhaar(val) => {
+      println!("Aadhaar No. - {}", val);
+    },
+    Identity::Passport(val) => {
+      println!("Passport ID - {}", val);
+    }
+  }
+}
+
+fn main() {
+  let p = Person {
+    name: String::from("Someone"),
+    last_name: String::from("Awesome"),
+    age: 201,
+    id: Identity::Passport(9863748),
+  };
+
+  let p2 = Person {
+    name: String::from("Someone"),
+    last_name: String::from("Awesome 2"),
+    age: 201,
+    id: Identity::Aadhaar("YoYoYo".to_string()),
+  };
+
+  p.print();
+  p2.print();
+  println!("------------------------");
+
+  check_identity(p.id);
+  println!("------------------------");
+
+  check_identity(p2.id);
+  println!("------------------------");
+
+  let a = Animal("Dog".to_string(), 10);
+  println!("{} - {}", a.0, a.1);
+  println!("------------------------");
+
+  let Animal(animal_name, _) = a;
+  println!("{}", animal_name);
+}
